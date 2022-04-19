1a721652213a89190b2a07a7d3747a3205ceb7ca
AnatolSalau <salovanatoly5@mail.ru>
2022-04-19 21:01:34 +0300

    Added CmdDebug.java

--- src/Lesson_1/Main.java
+++ src/Lesson_1/Main.java
@@ -1,9 +1,14 @@
 package Lesson_1;
 
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.util.Collection;
 
 //Наследуемся от класса Application
 public class Main extends Application {
@@ -18,11 +23,31 @@ public class Main extends Application {
 //создание коренвого узла
         Pane root = new Pane();
 
-//создаем сцену и указываем корневой узел - элемент где будут распологаться все поля, кнопки, итд
+//        Создадим кнопку
+        Button btn = new Button();
+        btn.setText("Click me");
+
+//        задаем позицию кнопки
+        btn.setTranslateX(140);
+        btn.setTranslateY(140);
+
+//      задаем размер кнопки
+        btn.setPrefSize(70,20);
+
+// создаем обработчик событий
+        CmdDebug cmdDebug = new CmdDebug();
+        btn.setOnAction(event ->{
+            cmdDebug.cmdPrint("Hello");
+                });
+
+//создаем сцену и ук
+// азываем корневой узел - элемент где будут распологаться все поля, кнопки, итд
         Scene scene = new Scene(root);
 
-//задаем сцену  для нашего окна
+//добавим кнопку на окно
+        root.getChildren().addAll(btn);
 
+//задаем сцену  для нашего окна
         primaryStage.setScene(scene);
 
 //        отобразим наше окно
